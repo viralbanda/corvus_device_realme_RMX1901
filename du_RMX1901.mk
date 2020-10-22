08c11bfb7ec802dbd1e41e61822fdd51097fa965
Ali Shahawez Khan (ViralBanda) <65936439+viralbanda@users.noreply.github.com>
2020-10-22 10:36:04 +0530

    coral oct fp

--- du_RMX1901.mk
+++ du_RMX1901.mk
@@ -24,8 +24,8 @@ TARGET_GAPPS_ARCH := arm64
 MOTORIZED_CAMERA_DEVICE := true
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="coral-user 10 QQ3A.200705.002 6506677 release-keys" \
+    PRIVATE_BUILD_DESC="coral-user 11 RP1A.201005.004 6782484 release-keys" \
     PRODUCT_NAME="RMX1901"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "google/coral/coral:10/QQ3A.200705.002/6506677:user/release-keys"
+BUILD_FINGERPRINT := google/coral/coral:11/RP1A.201005.004/6782484:user/release-keys
